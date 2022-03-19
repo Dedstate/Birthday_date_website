f4eec2b9fc76c45a61d79d66fbccc94d671eb46d
German Ivanov <germanivanov0719@gmail.com>
2022-03-20 02:01:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, 
+from flask import Flask, render_template, url_for
 from birthday_dates import a
 
 app = Flask(__name__)
@@ -11,8 +11,9 @@ def index():
     return render_template("index.html", a=a)
 
 
-app.add_url_rule('/favicon.ico',
-                 redirect_to=url_for('static', filename='favicon.png'))
+@app.route("/favicon.ico")
+def favicon():
+    return url_for("static", filename="favicon.png")
 
 
 if __name__ == "__main__":
