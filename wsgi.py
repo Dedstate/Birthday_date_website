411c748988bb6305c0325c5aad18bb185e08b221
German Ivanov <germanivanov0719@gmail.com>
2022-03-20 01:58:50 +0300

    Revert "Emergency fix 2"
    This reverts commit 6d0ef255b57f00728a4f907abb25809a8a07ef05.

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,4 @@
 from main import app
-from flask import url_for
 
 if __name__ == "__main__":
     app.run()
-    app.add_url_rule(
-        "/favicon.ico", redirect_to=url_for("static", filename="favicon.png")
-    )
