60025d9419a29b82ffb52ed162f050ebcf11ff27
German Ivanov <germanivanov0719@gmail.com>
2022-03-20 01:48:10 +0300

    Add favicon

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from birthday_dates import a
 
 app = Flask(__name__)
@@ -9,6 +9,11 @@ app = Flask(__name__)
 @app.route("/index.html")
 def index():
     return render_template("index.html", a=a)
+
+
+@app.route("/favicon.ico")
+def favicon():
+    return url_for("static", filename="favicon.png")
 
 
 if __name__ == "__main__":
