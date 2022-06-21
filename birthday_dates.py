69856eafb2f5888afce444916915456f47c7f750
German Ivanov <germanivanov0719@gmail.com>
2022-06-21 14:31:26 +0300

    Add support for people who left out class, major improvements, dynamic year calculation
    - Support archive (another page and new `birthdays_archived.csv` file)
    - Dynamic year calculation
    - Max year limiting
    - Better and more robust styles, now with `:root`
    - Add GitHub link
    - Remove `index.html` and replace with `base.html` for both pages
    - Update data

--- birthday_dates.py
+++ birthday_dates.py
@@ -1,7 +1,13 @@
 import csv
 
-a = []
+current = []
 with open("birthdays.csv", encoding="utf8") as csvfile:
     reader = csv.reader(csvfile, delimiter=",", quotechar='"')
     for index, row in enumerate(reader):
-        a.append(tuple(row))
+        current.append(tuple(row))
+
+archived = []
+with open("birthdays_archived.csv", encoding="utf8") as csvfile:
+    reader = csv.reader(csvfile, delimiter=",", quotechar='"')
+    for index, row in enumerate(reader):
+        archived.append(tuple(row))
--- main.py
+++ main.py
@@ -1,15 +1,50 @@
-from flask import Flask, render_template, url_for
-from birthday_dates import a
+from datetime import date
 
+from flask import Flask, render_template
+
+from birthday_dates import archived, current
+
+# Initialize Flask app and set constants
 app = Flask(__name__)
+LETTER = "Г"  # Letter appended to the year
+YEAR = 2013  # Year when started school
 
 
 @app.route("/")
+@app.route("/main")
 @app.route("/index")
+@app.route("/main.html")
 @app.route("/index.html")
-def index():
-    return render_template("index.html", a=a)
+def index() -> str:
+    return render_template("base.html", students=current, grade=grade)
+
+
+@app.route("/other")
+@app.route("/others")
+@app.route("/archive")
+@app.route("/history")
+@app.route("/archived")
+@app.route("/other.html")
+@app.route("/others.html")
+@app.route("/history.html")
+@app.route("/archive.html")
+@app.route("/archived.html")
+def history() -> str:
+    return render_template(
+        "base.html", students=archived, grade=grade, archived=True
+    )
+
+
+def get_grade_name(year: int, letter: str) -> str:
+    y = date.today().year - year
+    if date.today().month < 6:
+        y -= 1
+    grade = str(y) + letter
+    if y > 11:
+        grade = f"выпускников 11{LETTER} класса"
+    return grade
 
 
 if __name__ == "__main__":
+    grade = get_grade_name(YEAR, LETTER)
     app.run()
