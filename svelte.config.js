f76cc9d4429ca7697daf0ad1f9eda863e66d635d
German Ivanov <germanivanov0719@gmail.com>
2022-08-27 01:27:50 +0300

    Fix problem with GH Pages, optimize dark.css (use /docs for gh pages)

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,26 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-auto";
+// import preprocess from "svelte-preprocess";
 // was "@sveltejs/adapter-auto"
 
-const dev = "production" === "development";
+// const dev = "production" === "development";
+const dev = process.env.NODE_ENV === "development";
 
-/** @type {import(""@sveltejs/kit").Config} */
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
+  // preprocess: preprocess(),
   kit: {
     adapter: adapter(),
-    paths: {
-      // change below to your repo name
-      base: dev ? "" : "/birthday-dates",
-    },
     // hydrate the <div id="svelte"> element in src/app.html
     target: "#svelte",
+    trailingSlash: "ignore",
+    paths: {
+      assets: "docs",
+      pages: "docs",
+      base: "/birthday-dates",
+    },
   },
 };
+
+// const config = {};
 
 export default config;
